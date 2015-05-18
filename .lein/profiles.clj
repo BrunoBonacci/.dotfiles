bdeb2d903895b82e9895d06cf30e8e5bec56c1e2
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-05-18 22:20:15 +0100

    added leiningen profile

---
+++ .lein/profiles.clj
@@ -0,0 +1,15 @@
+{:user
+ {:plugins
+  [
+   [cider/cider-nrepl "0.8.1"]
+   [lein-midje "3.1.3"]
+   [lein-try "0.4.1"]
+   [lein-marginalia "0.7.1"]
+   [lein-bin "0.3.5"]
+   [lein-ancient "0.5.4"]
+   [jonase/eastwood "0.0.2"]
+   [lein-auto "0.1.1"]
+   ]
+  :dependencies
+  [[alembic "0.3.2"]
+   [criterium "0.4.3"]]}}
