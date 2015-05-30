fbf83f1bc8be13cc1f9bceab8cd026f8d4141508
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-05-30 19:03:46 +0100

    update prodigy settings

--- .prodigy.el
+++ .prodigy.el
@@ -2,27 +2,30 @@
 
 (require 'prodigy)
 
+;; reset status
+(setq prodigy-services '())
+
 ;;
 ;; SAMSARA
 ;; Ingestion API
 (setq prodigy-services
       (prodigy-define-service
-        :name "Samsara / Ingestion-APIs"
+        :name "Samsara / Ingestion-APIs / console"
         :cwd "/workspace/samsara/ingestion-api"
         :command "lein"
         :args '("run" "--" "-c" "./config/config.edn")
-        :tags '(samsara test)
+        :tags '(samsara)
         :port 9000
         :stop-signal 'kill
         :kill-process-buffer-on-stop t))
 
 (setq prodigy-services
       (prodigy-define-service
-        :name "Samsara / Ingestion-APIs / local kafka"
+        :name "Samsara / Ingestion-APIs / kafka"
         :cwd "/workspace/samsara/ingestion-api"
         :command "lein"
         :args '("run" "--" "-c" "./config/config-kafka.edn")
-        :tags '(samsara test)
+        :tags '(samsara sam-local)
         :port 9000
         :stop-signal 'kill
         :kill-process-buffer-on-stop t))
@@ -35,7 +38,7 @@
         :cwd "/workspace/exp/kafka"
         :command "/bin/bash"
         :args '("bin/zookeeper-server-start.sh" "config/zookeeper.properties")
-        :tags '(zookeeper kafka)
+        :tags '(zookeeper kafka sam-local)
         :port 2181
         :stop-signal 'kill
         :kill-process-buffer-on-stop t))
@@ -46,7 +49,7 @@
         :cwd "/workspace/exp/kafka"
         :command "/bin/bash"
         :args '("bin/kafka-server-start.sh" "config/server.properties")
-        :tags '(kafka)
+        :tags '(kafka sam-local)
         :port 9092
         :stop-signal 'kill
         :kill-process-buffer-on-stop t))
@@ -58,17 +61,30 @@
         :cwd "/workspace/exp/kafka"
         :command "/bin/bash"
         :args '("bin/kafka-console-consumer.sh" "--zookeeper" "localhost:2181" "--topic" "events")
-        :tags '(kafka)
+        :tags '(kafka sam-local)
         :stop-signal 'kill
         :kill-process-buffer-on-stop t))
 
 
 (setq prodigy-services
       (prodigy-define-service
-        :name "Kafka Consumer \"analyzed\" localhost"
+        :name "Kafka Consumer \"ingestion\" localhost"
         :cwd "/workspace/exp/kafka"
         :command "/bin/bash"
-        :args '("bin/kafka-console-consumer.sh" "--zookeeper" "localhost:2181" "--topic" "analyzed")
-        :tags '(kafka)
+        :args '("bin/kafka-console-consumer.sh" "--zookeeper" "localhost:2181" "--topic" "ingestion")
+        :tags '(kafka sam-local)
+        :stop-signal 'kill
+        :kill-process-buffer-on-stop t))
+
+;;
+;; SAMSARA CORE
+;;
+(setq prodigy-services
+      (prodigy-define-service
+        :name "Samsara / CORE / kafka"
+        :cwd "/workspace/samsara/core"
+        :command "lein"
+        :args '("run" "--" "-c" "./config/config.edn")
+        :tags '(samsara sam-local)
         :stop-signal 'kill
         :kill-process-buffer-on-stop t))
