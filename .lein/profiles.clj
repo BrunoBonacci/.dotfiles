b60d6f3facb4f1deafd6e55097064a9ad0a06562
Bruno Bonacci <bruno.bonacci@gmail.com>
2015-10-25 18:44:18 +0000

    update to new cider

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,7 +2,8 @@
  {:repl-options {:init (set! *print-length* 100)}
   :plugins
   [
-   [cider/cider-nrepl "0.9.1"]
+   [refactor-nrepl "2.0.0-SNAPSHOT"]
+   [cider/cider-nrepl "0.10.0-SNAPSHOT"]
    [lein-midje "3.1.3"]
    [lein-try "0.4.1"]
    [lein-marginalia "0.7.1"]
@@ -10,9 +11,9 @@
    [lein-ancient "0.5.4"]
    [jonase/eastwood "0.0.2"]
    [lein-auto "0.1.1"]
-   [refactor-nrepl "1.1.0"]
    ]
   :dependencies
   [[alembic "0.3.2"]
    [criterium "0.4.3"]
-   [org.clojure/tools.nrepl "0.2.10"]]}}
+   [org.clojure/tools.nrepl "0.2.11"]
+   ]}}
