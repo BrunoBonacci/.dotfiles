4c18edc9332cffee262e0778ef02d9bcfa857263
Bruno Bonacci <bruno.bonacci@gmail.com>
2017-01-25 21:48:03 +0000

    updated profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,23 +1,23 @@
 {:user
  {:repl-options {:init (set! *print-length* 100)}
+  :jvm-opts ["-server" "-Dfile.encoding=UTF-8"]
   :plugins
-  [[lein-midje "3.2"]
-   [lein-try "0.4.1"]
-   [lein-marginalia "0.7.1"]
-   [lein-binplus "0.4.1"]
-   [lein-ancient "0.5.4"]
-   [jonase/eastwood "0.2.1"]
-   [lein-auto "0.1.1"]
+  [[lein-auto "0.1.3"]]}
+ :repl
+ {:plugins
+  []
+  :dependencies
+  [[alembic "0.3.2"]
+   [criterium "0.4.4"]]}
+ :tools
+ {:plugins
+  [[lein-try "0.4.3"]
+   [lein-binplus "0.4.2"]
+   [lein-ancient "0.6.10"]
+   [jonase/eastwood "0.2.3"]
+   [lein-kibit "0.1.3"]
    [lein-shell "0.5.0"]
-   [lein-gorilla "0.3.6" :exclusions [[cider/cider-nrepl]]]
+   [lein-cloverage "1.0.7-SNAPSHOT"]
    #_[samsara/lein-template "0.1.0-SNAPSHOT"]
    ]
-  }
- :repl {:plugins
-        [[refactor-nrepl "2.2.0"]
-         [cider/cider-nrepl "0.13.0"]]
-        :dependencies
-        [[alembic "0.3.2"]
-         [criterium "0.4.3"]
-         #_[org.clojure/tools.nrepl "0.2.12"]
-         ]}}
+  }}
