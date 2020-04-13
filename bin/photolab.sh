ccc0988607d04bbd092b4419a87cbc8aa7dcbd8e
Bruno Bonacci <bruno.bonacci@gmail.com>
2020-04-13 11:09:49 +0100

    fix photolab compression

--- bin/photolab.sh
+++ bin/photolab.sh
@@ -7,10 +7,12 @@ function packit(){
   echo "Project: $PRJ  -- type: $2"
   cd $PRJ
   rm -fr ${PRJ}.md5
-  md5sum $( find . -type f ! -name \*.md5) > ${PRJ}.md5
+  rename 's/ /_/g' *
+  find . -type f ! -name \*.md5 | sort | xargs md5sum > ${PRJ}.md5
   cd ..
-  rar  a -r -rr10 -m1 -v1g $OUTDIR/${PRJ}${2}.rar $PRJ
+  rar  a -r -rr10 -m1 -v2g $OUTDIR/${PRJ}${2}.rar $PRJ
 }
 
 echo OUTDIR: $OUTDIR
+find $1 -name .DS_Store -delete
 packit $1 $2
