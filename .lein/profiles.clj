7689149e332c8e74d39aa7ed237b477853947214
Bruno Bonacci <bruno.bonacci@gmail.com>
2020-05-19 20:43:04 +0100

    update profile versions

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,24 +3,32 @@
   :jvm-opts ["-server" "-Dfile.encoding=UTF-8"]
   :plugins
   [[lein-auto "0.1.3"]]}
+
  :repl
  {:plugins
   []
+
   :dependencies
-  [[alembic "0.3.2"]
-   [criterium "0.4.4"]]}
+  [#_[alembic "0.3.2"]
+   #_[clj-commons/pomegranate "1.2.0"]
+   [criterium "0.4.5"]]}
+
+ :quality
+ {:plugins
+  [[jonase/eastwood "0.3.5"]
+   [lein-kibit "0.1.6"]
+   [lein-ancient "0.6.15"]
+   [lein-cloverage "1.1.1"]
+   [lein-nvd "1.0.0"]]}
+
  :tools
  {:plugins
-  [[lein-try "0.4.3"]
-   [lein-binplus "0.6.4"]
+  [[lein-binplus "0.6.4"]
    [lein-ancient "0.6.15"]
    [lein-shell "0.5.0"]
-   [lein-cloverage "1.0.10"]
    [lein-cljfmt "0.5.7"]
    [venantius/yagni "0.1.4"]
    [lein-bikeshed "0.5.1"]
-   [jonase/eastwood "0.2.6"]
-   [lein-kibit "0.1.6"]
    [lein-checkall "0.1.1"]
    #_[samsara/lein-template "0.1.0-SNAPSHOT"]
    ]
