1fdfb6a561d539bc436b575f3f736e7837a5e55e
Bruno Bonacci <bruno.bonacci@gmail.com>
2020-10-10 20:45:25 +0100

    update profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,45 +1,69 @@
 {:user
  {:repl-options {:init (set! *print-length* 100)}
   :jvm-opts ["-server" "-Dfile.encoding=UTF-8"]
-  :plugins
-  [[lein-auto "0.1.3"]]}
+  :dependencies [[com.brunobonacci/where "0.5.5"]
+                 [cider/cider-nrepl "0.25.4"]]
 
- :repl
+  :plugins
+  [[lein-auto "0.1.3"]
+   [refactor-nrepl "2.5.1"]]
+
+  :aliases
+  {"clean-clojure"
+   ["with-profile" "tools" "cljfmt" "fix"]}}
+
+
+ :dev
  {:plugins
-  []
+  [[lein-jmh "0.2.8"]
+   [lein-midje "3.2.2"]
+   ]
 
   :dependencies
   [#_[alembic "0.3.2"]
    #_[clj-commons/pomegranate "1.2.0"]
-   [criterium "0.4.5"]]}
+   [criterium "0.4.6"]
+   [com.clojure-goes-fast/clj-async-profiler "0.4.1"]
+   [jmh-clojure "0.3.1"]
+   ]
+
+  }
+
 
  :quality
  {:plugins
-  [[jonase/eastwood "0.3.5"]
-   [lein-kibit "0.1.6"]
-   [lein-ancient "0.6.15"]
-   [lein-cloverage "1.1.1"]
-   [lein-nvd "1.0.0"]]}
+  [[jonase/eastwood "0.3.10"] ;; code smells / linting
+   [lein-kibit      "0.1.6"]  ;; check idiomatic clojure
+   [lein-ancient    "0.6.15"] ;; check/update dependencies
+   [lein-cloverage  "1.2.0"]  ;; code coverage
+   [lein-nvd        "1.4.1"]  ;; security scan
+   [venantius/yagni "0.1.4"]  ;; dead code elimination
+   [lein-bikeshed   "0.5.1"]  ;; check code smells
+   [lein-checkall   "0.1.1"]  ;; like lein check, kibit, eastwood bikeshed
+   ]}
 
  :tools
  {:plugins
-  [[lein-binplus "0.6.4"]
-   [lein-ancient "0.6.15"]
-   [lein-shell "0.5.0"]
-   [lein-cljfmt "0.5.7"]
-   [venantius/yagni "0.1.4"]
-   [lein-bikeshed "0.5.1"]
-   [lein-checkall "0.1.1"]
+  [[lein-binplus "0.6.4"]    ;; build executable jars
+   [lein-ancient "0.6.15"]   ;; check/update dependencies
+   [lein-shell   "0.5.0"]    ;; run shell commands
+   [lein-cljfmt  "0.6.8"]    ;; source code formatting
    #_[samsara/lein-template "0.1.0-SNAPSHOT"]
    ]
+
+  :cljfmt {:remove-consecutive-blank-lines? false
+           :remove-surrounding-whitespace?  false
+           :indents ^:replace {#".*"   [[:inner 0]]}}
   }
  :tools2
  {:plugins
   [[com.livingsocial/lein-dependency-check "1.0.2"] ;; security scan
-   [lein-instant-cheatsheet "2.2.2"]
-   [org.timmc/nephila "0.3.0"] ;; namespace dep graph
+   [lein-instant-cheatsheet "2.2.2"]   ;; create checksheets for your projects
+   [org.timmc/nephila "0.3.0"]         ;; namespace dep graph
    ]
-  }}
+  }
+
+ }
 
 
 ;; lein-extend-cp - local lib dir
---
+++ babashka.clj
@@ -0,0 +1,71 @@
+(require
+  '[where.core :refer [where]]
+  '[cheshire.core :as json]
+  '[clojure.string :as str]
+  '[clojure.java.io :as io]
+  '[clojure.java.shell :as sh]
+  '[babashka.curl :as curl])
+
+
+(defn printerr
+  [& args]
+  (binding [*out* *err*]
+    (apply println args)))
+
+
+
+(defn sys-env
+  []
+  (into {} (System/getenv)))
+
+
+
+(defn sh
+  [cmd & {:keys [dir env]}]
+  (shell/sh "bash" "-c" cmd :dir dir :env (merge (sys-env) env)))
+
+
+
+(defn sh!
+  [cmd & {:keys [dir env hide?]}]
+  (when-not hide?
+    (println cmd))
+  (let [{:keys [out err exit] :as p} (sh cmd :dir dir :env env)]
+    (when (not (empty? out))
+      (println out))
+    (when (not (empty? err))
+      (printerr err))
+    (when (> exit 0)
+      (throw (ex-info (str "Command error: " (when-not hide? cmd)) p)))
+    out))
+
+
+
+(defn shf!
+  "Execute a command-template as in `format` rendered with the arguments"
+  {:arglists '([cmd-template & args] [{:keys [dir env hide?]} cmd-template & args])}
+  [cmd1 & [cmd2 & args2 :as args]]
+  (if (map? cmd1)
+    (sh! (apply format cmd2 args2) :dir (:dir cmd1) :env (:env cmd1) :hide? (:hide? cmd1))
+    (sh! (apply format cmd1 args))))
+
+
+
+(defn from-json
+  [s]
+  (json/parse-string s true))
+
+
+(defn to-json
+  [s]
+  (json/generate-string s))
+
+
+(defn now
+  []
+  (System/currentTimeMillis))
+
+
+(defn uuid
+  []
+  (str (java.util.UUID/randomUUID)))
